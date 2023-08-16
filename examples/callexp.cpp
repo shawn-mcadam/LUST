cb311faf815e8c4cb7609e6c9fd9e41ca676756f
Shawn McAdam <ii1mcadam@gmail.com>
2023-08-16 16:55:57 -0600

    test source

---
+++ examples/callexp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "lust.hpp"
+
+int main(){
+  //lust::detail::exp2lutf.print_json(std::cout);
+  //lust::detail::exp2lutd.print_json(std::cout);
+  //lust::detail::log2lutf.print_json(std::cout);
+  //lust::detail::log2lutd.print_json(std::cout);
+
+  std::cout << lust::pow(3.14,3.0) << std::endl;
+}
